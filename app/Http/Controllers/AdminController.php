e4096900d4ce81e808a9e9abb6682fedbe3a8eb8
JoynalAbedin3 <engr.joynal.bd@gmail.com>
2023-08-25 23:45:11 +0600

    admin profile-2

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\RedirectResponse;
+use App\Models\User;
 
 class AdminController extends Controller
 {
@@ -15,6 +16,15 @@ class AdminController extends Controller
     public function AdminLogin(){
         return view('admin.admin_login');
     }
+
+    public function AdminProfile(){
+        $id = Auth::user()->id;
+        $adminData = User::find($id);
+
+        return view('admin.admin_profile_view');
+    }
+
+
 
     public function AdminDestroy(Request $request): RedirectResponse
     {
